4e9f1e6b4928005e2c6c732f7f6ee90a535a7131
benja2998 <197428407+benja2998@users.noreply.github.com>
2025-06-29 21:21:11 -0400

    Update report.ts

--- api/report.ts
+++ api/report.ts
@@ -8,6 +8,27 @@ export default function handler(req: VercelRequest, res: VercelResponse) {
 
   const { name, email, software, description } = req.body || {}
 
+  // Check if the description is atleast 10 characters long
+  if (description && description.length < 10) {
+    res.status(400).send('Description must be at least 10 characters long')
+    return
+  }
+
+  // Remove <> to avoid injection
+  const sanitizedDescription = description.replace(/<[^>]*>/g, '')
+  const sanitizedName = name.replace(/<[^>]*>/g, '')
+  const sanitizedEmail = email.replace(/<[^>]*>/g, '')
+  const sanitizedSoftware = software.replace(/<[^>]*>/g, '')
+
+  // Check if the software is one of the allowed values
+
+  const allowedSoftware = ['kali-in-batch', 'peroxide-h', 'linux-on-batch-windows']
+
+  if (!allowedSoftware.includes(software)) {
+    res.status(400).send('Invalid software value')
+    return
+  }
+
   console.log('Received form data:')
   console.log('Name:', name)
   console.log('Email:', email)
